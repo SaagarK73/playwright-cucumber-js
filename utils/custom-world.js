8ab6c9ed613e99a577c5dda2298acf42d8985c0e
unknown <saagarkalaskar@gmail.com>
2024-08-27 17:49:24 +0530

    adding actions

--- utils/custom-world.js
+++ utils/custom-world.js
@@ -9,7 +9,7 @@ class CustomWorld extends World{
   }
 
   async setUp() {
-    this.browser = await chromium.launch({ headless: false });
+    this.browser = await chromium.launch({ headless: true });
     this.context = await this.browser.newContext();
     this.page = await this.context.newPage();
     this.appPages = new PageObjects(this.page)
